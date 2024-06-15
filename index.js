8298042ec03f86433493f432c86c203eaf0083d4
Mukhammedyar <116536513+Mukhammedyar@users.noreply.github.com>
2024-06-15 17:03:30 +0500

    nodemon 15.06.2024

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const http = require("http");
 
 const server = http.createServer((request, response) => {
-  console.log(request.url);
-  response.write("<h1> hello from http server </h1>");
+  //   console.log(request.url);
+  response.write("<h1> hello from http server 2233</h1>");
   response.end();
 });
 
-server.listen(3000, () => {
+server.listen(8080, () => {
   console.log("server starting on 3000");
 });
