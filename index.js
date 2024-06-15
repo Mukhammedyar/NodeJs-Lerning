4ea41f0c6c7719c871a3ca8edc85317aa021382e
Mukhammedyar <116536513+Mukhammedyar@users.noreply.github.com>
2024-06-15 17:46:31 +0500

    web server 14-lesson 15.06.2024

--- index.js
+++ index.js
@@ -1,9 +1,33 @@
 const http = require("http");
 
-const server = http.createServer((request, response) => {
+const server = http.createServer((req, res) => {
   //   console.log(request.url);
-  response.write("<h1> hello from http server 2233</h1>");
-  response.end();
+  //   response.write("<h1> hello from http server 2233</h1>");
+  //   response.end();
+  //   response.end("<h1> hello from http server 223q3</h1>");
+
+  if (req.method === "GET") {
+    res.writeHead(200, { "Content-type": "text/html" });
+    res.end(`
+          <h2> hello from http server</h2>
+          <form method="post" action="/">
+            <input type="email" name="email" placeholder="email"/>
+            <button type="submit">Submin </button>
+          </form>
+      `);
+  } else if (req.method === "POST") {
+    res.writeHead(200, { "Content-type": "text/html; charset=utf-8" });
+    const email = [];
+
+    req.on("data", (data) => {
+      email.push(Buffer.from(data));
+    });
+
+    req.on("end", () => {
+      let message = email.toString().split("=")[1];
+      res.end(`email succefully sent: ${message}`);
+    });
+  }
 });
 
 server.listen(8080, () => {
